9266c59c06d858ed225ea5e507fe0ae2dc4b0117
jlkiri <interalia.dev@gmail.com>
2021-06-16 18:33:03 +0900

    ipv6

--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,13 @@ use server::Server;
 use structopt::StructOpt;
 
 #[derive(StructOpt, Debug)]
-#[structopt(name = "rnc")]
+#[structopt(name = "teacup")]
 enum Opt {
     Listen {
         #[structopt(short, long)]
         port: Option<u16>,
+        #[structopt(long)]
+        ipv6: bool
     },
     Connect {
         #[structopt(short, long)]
@@ -20,13 +22,20 @@ enum Opt {
     },
 }
 
+fn local_addr(port: u16, ipv6: bool) -> SocketAddr {
+  if ipv6 {
+    return SocketAddr::new(IpAddr::V6(Ipv6Addr::new(0, 0, 0, 0, 0, 0, 0, 1)), port);
+  }
+
+  SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), port)
+}
+
 fn main() -> std::io::Result<()> {
     let opt = Opt::from_args();
 
     match opt {
-        Opt::Listen { port } => {
-            let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), port.unwrap_or(3000));
-            // let addr = SocketAddr::new(IpAddr::V6(Ipv6Addr::new(0, 0, 0, 0, 0, 0, 0, 1)), port.unwrap_or(3000));
+        Opt::Listen { port, ipv6 } => {
+            let addr = local_addr(port.unwrap_or(8888), ipv6);
             let server = Server::new(addr);
             server.listen()?;
         }

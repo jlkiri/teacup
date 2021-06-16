8351a4dee134d73d0004c76b03a0218b8a0d8d5e
jlkiri <interalia.dev@gmail.com>
2021-06-16 21:56:51 +0900

    refactor

--- src/main.rs
+++ src/main.rs
@@ -6,26 +6,62 @@ use std::net::{IpAddr, Ipv4Addr, Ipv6Addr, SocketAddr};
 
 use client::Client;
 use server::Server;
-use udp::UdpServer;
 use structopt::StructOpt;
+use udp::{UdpClient, UdpServer};
+
+#[derive(Debug)]
+enum Protocol {
+    Tcp,
+    Udp,
+}
+
+impl From<&str> for Protocol {
+    fn from(string: &str) -> Self {
+        match string {
+            "udp" => Self::Udp,
+            "tcp" => Self::Tcp,
+            _ => panic!("Unknown protocol."),
+        }
+    }
+}
+
+impl From<Protocol> for String {
+    fn from(protocol: Protocol) -> String {
+        match protocol {
+            Protocol::Udp => String::from("udp"),
+            Protocol::Tcp => String::from("tcp"),
+        }
+    }
+}
+
+const UDP: &str = "udp";
+const TCP: &str = "tcp";
+
+#[derive(StructOpt, Debug)]
+struct ClientOptions {
+    #[structopt(short, long)]
+    addr: String,
+    #[structopt(short, long, default_value = TCP, parse(from_str))]
+    protocol: Protocol,
+    #[structopt(short, long, required_if("protocol", UDP))]
+    data: Option<String>,
+}
+
+#[derive(StructOpt, Debug)]
+struct ServerOptions {
+    #[structopt(short, long)]
+    port: Option<u16>,
+    #[structopt(long)]
+    ipv6: bool,
+    #[structopt(long, default_value = TCP, parse(from_str))]
+    protocol: Protocol,
+}
 
 #[derive(StructOpt, Debug)]
 #[structopt(name = "teacup")]
-enum Opt {
-    Listen {
-        #[structopt(short, long)]
-        port: Option<u16>,
-        #[structopt(long)]
-        ipv6: bool,
-        #[structopt(short, long)]
-        udp: bool,
-    },
-    Connect {
-        #[structopt(short, long)]
-        addr: String,
-        #[structopt(short, long)]
-        udp: bool,
-    },
+enum Teacup {
+    Listen(ServerOptions),
+    Connect(ClientOptions),
 }
 
 fn local_addr(port: u16, ipv6: bool) -> SocketAddr {
@@ -37,26 +73,37 @@ fn local_addr(port: u16, ipv6: bool) -> SocketAddr {
 }
 
 fn main() -> std::io::Result<()> {
-    let opt = Opt::from_args();
+    let opt = Teacup::from_args();
 
     match opt {
-        Opt::Listen { port, ipv6, udp: true } => {
-          let addr = local_addr(port.unwrap_or(8888), ipv6);
-          UdpServer::listen(addr)?
+        Teacup::Listen(ServerOptions {
+            port,
+            ipv6,
+            protocol,
+        }) => match protocol {
+            Protocol::Tcp => {
+                let addr = local_addr(port.unwrap_or(8888), ipv6);
+                let server = Server::new(addr);
+                server.listen()?
+            }
+            Protocol::Udp => {
+                let addr = local_addr(port.unwrap_or(8888), ipv6);
+                UdpServer::listen(addr)?
+            }
         },
-        Opt::Listen { port, ipv6, udp: false } => {
-            let addr = local_addr(port.unwrap_or(8888), ipv6);
-            let server = Server::new(addr);
-            server.listen()?
+        Teacup::Connect(ClientOptions {
+            addr,
+            protocol,
+            data,
+        }) => match protocol {
+            Protocol::Tcp => {
+                let client = Client::new(addr);
+                client.connect()?
+            }
+            Protocol::Udp => {
+                UdpClient::send(addr, data.unwrap_or("Hello!".into()));
+            }
         },
-        Opt::Connect { addr, udp: true } => {
-            let client = Client::new(addr);
-            client.connect()?
-        },
-        Opt::Connect { addr, udp: false } => {
-          let client = Client::new(addr);
-          client.connect()?
-      }
     }
 
     Ok(())
--- src/udp.rs
+++ src/udp.rs
@@ -1,27 +1,37 @@
 use std::{
-  io::{Read, Write},
-  net::*,
-  thread, vec,
+    io::{Read, Write},
+    net::*,
+    thread, vec,
 };
 
 const BUFFER_SIZE: usize = 32;
 
 fn handle_connection(socket: UdpSocket) -> std::io::Result<()> {
-  let mut buf = [0u8; BUFFER_SIZE];
+    let mut buf = [0u8; BUFFER_SIZE];
 
-  let (len, addr) = socket.recv_from(&mut buf)?;
+    let (len, addr) = socket.recv_from(&mut buf)?;
 
-  println!("Received a UDP packet from {}", addr);
-  println!("Content: {}", String::from_utf8_lossy(&buf[..len]));
+    println!("Received a UDP packet from {}", addr);
+    println!("Content: {}", String::from_utf8_lossy(&buf[..len]));
 
-  Ok(())
+    Ok(())
 }
 
 pub struct UdpServer;
+pub struct UdpClient;
 
 impl UdpServer {
     pub fn listen<A: ToSocketAddrs>(addr: A) -> std::io::Result<()> {
         let socket = UdpSocket::bind(addr)?;
         handle_connection(socket)
+    }
+}
+
+impl UdpClient {
+    pub fn send<A: ToSocketAddrs>(addr: A, data: String) {
+        let socket = UdpSocket::bind("127.0.0.1:9999").expect("Failed to bind.");
+        socket
+            .send_to(data.as_bytes(), addr)
+            .expect("Failed to send UDP message.");
     }
 }

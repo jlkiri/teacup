4e7e8d0c5d2b01ab9942cf8dd8b6b978b0137229
jlkiri <interalia.dev@gmail.com>
2021-06-20 22:43:39 +0900

    simplify with try_clone

--- src/connection.rs
+++ src/connection.rs
@@ -1,24 +1,19 @@
 use std::io::{ErrorKind, Read, Result, Write};
 use std::net::TcpStream;
-use std::sync::{Arc, Mutex, mpsc};
-use std::thread;
+use std::thread::{self, JoinHandle};
 
-fn read_stdin(stream: Arc<Mutex<TcpStream>>) {
-    // let copystream = Arc::clone(&stream);
+fn write_from_stdin(mut w: TcpStream) -> JoinHandle<()> {
     thread::spawn(move || loop {
         let mut input = String::new();
         std::io::stdin().read_line(&mut input).unwrap();
-        let mut w = stream.lock().unwrap();
         w.write_all(input.as_bytes()).unwrap();
         w.flush().unwrap();
-    });
+    })
 }
 
-fn write_stdout(stream: Arc<Mutex<TcpStream>>) -> Result<()> {
-    let mut buf = [0u8; 128];
-
-    loop {
-        let mut r = stream.lock().unwrap();
+fn read_to_stdout(mut r: TcpStream) -> JoinHandle<()> {
+    thread::spawn(move || loop {
+        let mut buf = [0u8; 128];
         match r.read(&mut buf) {
             Ok(0) => break,
             Ok(len) => {
@@ -30,27 +25,22 @@ fn write_stdout(stream: Arc<Mutex<TcpStream>>) -> Result<()> {
                 );
             }
             Err(e) => {
+                println!("Err.");
                 match e.kind() {
                     ErrorKind::Interrupted | ErrorKind::WouldBlock => continue,
-                    _ => return Err(e)
+                    _ => panic!("{}", e),
                 }
             }
         }
-    }
-
-    Ok(())
+    })
 }
 
-pub fn handle_connection(strm: TcpStream) -> Result<()> {
-    let (tx, rx) = mpsc::channel::<String>();
-    let peer_addr = strm.peer_addr()?;
+pub fn handle_connection(stream: TcpStream) -> Result<()> {
+    let t1 = write_from_stdin(stream.try_clone()?);
+    let t2 = read_to_stdout(stream);
 
-    let stream = Arc::new(Mutex::new(strm));
-
-    stream.lock().unwrap().set_nonblocking(true)?;
-
-    read_stdin(Arc::clone(&stream));
-    write_stdout(Arc::clone(&stream));
+    t1.join().unwrap();
+    t2.join().unwrap();
 
     Ok(())
 }
